1de4ae9f449924db364c2eba817aa54467ef0f7b
tfredy2 <you@example.com>
2022-02-14 13:57:33 -0600

    ahora se puede cargar un archivo csv

--- index.py
+++ index.py
@@ -1,40 +1,74 @@
 import cx_Oracle
 import os
-import time
-
-def verifica_directorio():
-    """
-    Verifica si existe el directorio sino lo crea
-    """
-    if not os.path.isdir("C:\\respaldo_sql"):
-        os.mkdir("C:\\respaldo_sql")
-        print("El directorio no Existe y se creo y esta en: C:\\respaldo_sql")
-
-def respaldopkg():
-    """
-    Realiza el respaldo de los procedimientos proporcionados
-    """
-    dsn = cx_Oracle.makedsn(host='127.0.0.1', port=1521, sid='mi_base')
-    conexion = cx_Oracle.connect(user='user', password='root', dsn=dsn,encoding='UTF-8', nencoding='UTF-8')
-
-    nombre_pkg = input("Ingresa nombre del package a respaldar: \n")
-    lista = nombre_pkg.split(",")
-    for i in range(0,int(len(lista))):
-        cursor = conexion.cursor()
-        cursor.execute(f"select dbms_metadata.get_ddl('PACKAGE_SPEC', '{lista[i].upper()}', 'user'),dbms_metadata.get_ddl('PACKAGE_BODY', '{lista[i].upper()}', 'user')  from dual")
-
-        files = open(f"C:\\respaldo_sql\\{lista[i].upper()}-{str(time.strftime('%d-%m-%Y_%H%M%S'))}.sql","w")
-        for j in cursor:
-            files.write(str(j[0])+"\n\n")    
-            files.write(str(j[1]))
-    
-        print("Se respaldo el package:",lista[i],"correctamente")
-    input("Preciona cualquier tecla para cerrar")
-
-    files.close()
-    conexion.close()
+from datetime import datetime
+import csv
 
 
-if __name__ == "__main__":
-    verifica_directorio()
-    respaldopkg()
+class SavePackages:
+    def __init__(self):
+        self.path = 'C:\\respaldo_sql\\'
+        self.user = 'root'
+        self.pwd = 'your_psw'
+        self.host = '127.0.0.1'
+        self.sid = 'mi_base'
+        self.port = 1521
+        self.dns = cx_Oracle.makedsn(host=self.host, port=self.port, sid=self.sid)        
+        self.menu()
+
+    def verify_directory(self):
+        try:
+            if not os.path.isdir(self.path):
+                os.mkdir(self.path)
+                print(f'El directorio no existe se creo en: {self.path}')
+        except Exception as ex:
+            print(ex)
+
+    def save_backup(self,item=None):
+        try:
+            if item is None:
+                name_pkg = input('Ingresa nombre del package a respaldar puede ser mas de uno separado por (,): \n').split(',')
+            else:
+                name_pkg = item            
+            connection = cx_Oracle.connect(user=self.user, password=self.pwd, dsn=self.dns, encoding='utf-8', nencoding='utf-8')
+            with connection as conexion:
+                for pkg in name_pkg:
+                    date = datetime.strftime(datetime.now(), '%d-%m-%Y_%H_%M_%S')
+                    if pkg.strip() is not '':
+                        with conexion.cursor() as cursor:
+                            for row in cursor.execute(f"select dbms_metadata.get_ddl('PACKAGE_SPEC', '{pkg.strip().upper()}', '{self.user}'),dbms_metadata.get_ddl('PACKAGE_BODY', '{pkg.strip().upper()}', '{self.user}')  from dual"):                                
+                                with open(f'{self.path}{pkg.strip().upper()}{date}.sql', 'w', encoding='utf-8') as file:
+                                    file.write(str(row[0]).replace('"', '').strip()+'\n/\n')
+                                    file.write(str(row[1]).replace('"', '').strip()+'\n/\n')
+                                    print(f'Se realizo el respaldo del package: {pkg.strip().upper()}{date}')
+        except Exception as ex:
+            print(ex)
+
+    def read_csv(self):
+        try:
+            path_file = input('Ingresa la ruta del archivo csv:\n\t')
+            if os.path.isfile(path_file) and os.path.splitext(path_file)[1].upper()  == '.CSV':
+                with open(path_file, 'r',encoding='utf-8',) as file_csv:
+                    reader = csv.reader(file_csv.readlines())
+                    for ls in reader:
+                        self.save_backup(ls)
+            else:
+                print('Verifique que el archivo exista o sea en formato .CSV')
+        except Exception as ex:
+            print(ex)
+            
+    def menu(self):
+        try:            
+            while True:
+                opcion = int(input('Script para realizar respaldos de packages en BD ORACLE ingresa:\n\n1.- ingresar nombre de packages\n2.- Cargar CSV, se debe dar la ruta del archivo\n\t'))
+                if opcion > 0 and opcion < 3:
+                    if opcion == 1:
+                        self.save_backup()
+                    elif opcion == 2:
+                        self.read_csv()
+                    break
+                print('Opcion no valida\n')
+        except Exception as ex:
+            print(ex)
+
+if __name__ == '__main__':
+    SavePackages()
